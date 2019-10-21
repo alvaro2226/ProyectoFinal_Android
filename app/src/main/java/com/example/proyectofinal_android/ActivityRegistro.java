df8b247f955628403835110e71a427e6b8557b87
alvaro2226 <morcyandcompany@gmail.com>
2019-10-21 16:46:58 +0200

    Empiezo a crear la activity para registrar un usuario

---
+++ app/src/main/java/com/example/proyectofinal_android/ActivityRegistro.java
@@ -0,0 +1,13 @@
+package com.example.proyectofinal_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityRegistro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
