e6fca96055cbefa32d14834e22171795b64a2a14
alvaro2226 <morcyandcompany@gmail.com>
2019-10-22 14:06:07 +0200

    Algunos cambios

--- app/src/main/java/com/example/proyectofinal_android/ActivityLogin.java
+++ app/src/main/java/com/example/proyectofinal_android/ActivityLogin.java
@@ -1,15 +1,29 @@
 package com.example.proyectofinal_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ActivityLogin extends AppCompatActivity {
 
+    private EditText editText_nombreUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViews();
+
+        //editText_nombreUsuario.setText(getIntent().getExtras().getString("nombreUsuario",""));
 
 
+    }
+
+    private void initViews(){
+        editText_nombreUsuario =findViewById(R.id.editTextUname);
+    }
+    public void iniciarIntentRegistro(View view) {
+        startActivity(new Intent(this,ActivityRegistro.class));
     }
 }
--- app/src/main/java/com/example/proyectofinal_android/ActivityRegistro.java
+++ app/src/main/java/com/example/proyectofinal_android/ActivityRegistro.java
@@ -1,13 +1,30 @@
 package com.example.proyectofinal_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ActivityRegistro extends AppCompatActivity {
 
+    private EditText editText_nombreUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        initViews();
+    }
+
+    private void initViews() {
+        editText_nombreUsuario = findViewById(R.id.editText_nombreUsuario);
+    }
+
+    public void confirmarRegistro(View view) {
+
+        //Si los datos son correctos
+        Intent intent = new Intent(this, ActivityLogin.class);
+        intent.putExtra("nombreUsuario", editText_nombreUsuario.getText().toString());
+        startActivity(intent);
     }
 }
